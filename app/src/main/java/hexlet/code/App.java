b72b7a65df5e037c0b6b53272c5fc78578082c32
Irina Komarchenko <irinakomarchenko@gmail.com>
2024-04-12 21:23:23 +0200

    Added 2 -game Even

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,33 @@
 package hexlet.code;
 
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) {
-        Cli.startGame();
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter the game number and press Enter.");
+        System.out.println("1 - Greet");
+        System.out.println("2 - Even");
+        System.out.println("0 - Exit");
+        System.out.print("Your choice: ");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                System.out.println("Welcome to the Brain Games!");
+                System.out.print("May I have your name? ");
+                String name = scanner.next();
+                System.out.println("Hello, " + name + "!");
+                break;
+            case 2:
+                Even even = new  Even();
+                even.play();
+                break;
+            case 0:
+                System.out.println("Goodbye!");
+                break;
+            default:
+                System.out.println("Invalid choice. Please enter 1 or 0.");
+        }
     }
 }
--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,11 +2,18 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli {
+
     public static void startGame() {
+        //select game 1 or 2
+        sayHello(); // only if 1
+    }
+    public static void sayHello() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Welcome to the Brain Games!");
         System.out.print("May I have your name? ");
         String name = scanner.nextLine();
         System.out.println("Hello, " + name + "!");
     }
+
+    //method - game 2 {sayHello()...}
 }
